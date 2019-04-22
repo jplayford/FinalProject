a3b0930b293ccc929324e8cb1308062cb313e417
Joan <jplayford@cshnyc.org>
2019-04-22 17:47:37 -0400

    Initial commit from Repl.it

---
+++ IJS-math/script.js
@@ -0,0 +1,212 @@
+//Values for all  elements
+var singleToe = 0.4;
+var singleSal = 0.4;
+var singleLoop = 0.5;
+var singleEuler = 0.5;
+var singleFlip = 0.5;
+var singleLutz = 0.6;
+var singleAxel = 1.1;
+var doubleSal = 1.3;
+var doubleToe = 1.3;
+var doubleLoop = 1.8;
+var doubleFlip = 1.9;
+var doubleLutz = 2.1;
+var doubleAxel = 3.3;
+var tripleSal = 4.4;
+var tripleToe = 4.3;
+var tripleLoop = 5.1;
+var tripleFlip = 5.3;
+var tripleLutz = 6.0;
+var tripleAxel = 8.5;
+
+
+//buttons with return values for all elements
+var button = document.createElement("button");
+button.innerHTML = "Single Toeloop";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return singleToe;
+});
+
+var button = document.createElement("button");
+button.innerHTML = "Single Salchow";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return singleSal;
+});
+
+var button = document.createElement("button");
+button.innerHTML = "Single Loop";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return singleLoop;
+});
+
+var button = document.createElement("button");
+button.innerHTML = "Single Euler";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return singleEuler;
+});
+
+var button = document.createElement("button");
+button.innerHTML = "Single Flip";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return singleFlip;
+});
+
+var button = document.createElement("button");
+button.innerHTML = "Single Lutz";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return singleLutz;
+});
+
+var button = document.createElement("button");
+button.innerHTML = "Single Axel";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return singleAxel;
+});
+
+var button = document.createElement("button");
+button.innerHTML = "Double Toeloop";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return doubleToe;
+});
+
+var button = document.createElement("button");
+button.innerHTML = "Double Salchow";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return doubleSal;
+});
+
+var button = document.createElement("button");
+button.innerHTML = "Double Loop";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return doubleLoop;
+});
+
+var button = document.createElement("button");
+button.innerHTML = "Double Flip";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return doubleFlip;
+});
+
+var button = document.createElement("button");
+button.innerHTML = "Double Lutz";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return doubleLutz;
+});
+
+var button = document.createElement("button");
+button.innerHTML = "Double Axel";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return doubleAxel;
+});
+
+var button = document.createElement("button");
+button.innerHTML = "Triple Toeloop";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return tripleToe;
+});
+
+var button = document.createElement("button");
+button.innerHTML = "Triple Salchow";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return tripleSal;
+});
+
+var button = document.createElement("button");
+button.innerHTML = "Triple Loop";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return tripleLoop;
+});
+
+var button = document.createElement("button");
+button.innerHTML = "Triple Flip";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return tripleFlip;
+});
+
+var button = document.createElement("button");
+button.innerHTML = "Triple Lutz";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return tripleLutz;
+});
+
+var button = document.createElement("button");
+button.innerHTML = "Triple Axel";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  return tripleAxel;
+});
